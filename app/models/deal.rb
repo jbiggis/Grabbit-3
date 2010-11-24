0626cd7c62fe1daceac6c8cd91d3b3b62d4c5d31
Jonathan Chiu <jscchiu@gmail.com>
2010-11-24 02:14:05 -0500

    added image upload functionality using paperclip

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,4 +2,7 @@ class Deal < ActiveRecord::Base
 
 	has_many :coupons
 
+	has_attached_file :image, :url => "/assets/deals/:id/:style/:basename.:extension",
+	:path => ":rails_root/public/assets/deals/:id/:style/:basename.:extension"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101124033447) do
+ActiveRecord::Schema.define(:version => 20101124062319) do
 
   create_table "coupons", :force => true do |t|
     t.integer  "coupon_number"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(:version => 20101124033447) do
     t.datetime "end_time"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_path"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "orders", :force => true do |t|
